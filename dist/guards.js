0d0dc56daae97d4893b81ffb59f39a1af52fe4a3
Rajeev-Foryou <mandalrajeev3@gmail.com>
2026-01-08 14:39:11 +0530

    Update .gitignore to ignore dist and node_modules

--- dist/guards.js
+++
@@ -1,9 +0,0 @@
-export function isChaiOrder(value) {
-    return (typeof value === "object" &&
-        value !== null &&
-        "id" in value &&
-        "size" in value &&
-        typeof value.id === "number" &&
-        ["small", "medium", "Large"].includes(value.size));
-}
-//# sourceMappingURL=guards.js.map
--- dist/order.js
+++
@@ -1,5 +0,0 @@
-export function prepareChai(order) {
-    const sugarText = order.sugar ? `with${order.sugar} sugar` : "without sugar";
-    return `Preparing ${order.size} chai ${sugarText}`;
-}
-//# sourceMappingURL=order.js.map
